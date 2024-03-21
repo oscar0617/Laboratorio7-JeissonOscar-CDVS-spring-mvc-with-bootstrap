c524a690722c8240462fe854e561de3b78a33ae2
Jorge Ulises Useche Cuellar <jorge.useche@nubank.com.br>
2024-03-21 17:41:57 -0500

    Adding access to database in one-line with only one env

--- src/test/java/co/edu/escuelaing/cvds/lab7/Lab7ApplicationTests.java
+++ src/test/java/co/edu/escuelaing/cvds/lab7/Lab7ApplicationTests.java
@@ -1,13 +1,14 @@
 package co.edu.escuelaing.cvds.lab7;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+/*import org.junit.jupiter.api.Test;*/
 
 @SpringBootTest
 class Lab7ApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
